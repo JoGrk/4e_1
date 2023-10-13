e7ee03b7eadaa30c533f8c019f9045083acc792b
Jola Gryko <jgryko@gmail.com>
2023-10-13 12:42:21 +0200

    norm

---
+++ bd/06_DDL_vehicles.sql
@@ -0,0 +1,15 @@
+-- Utwórz tabelę vehicles : pole vehicledId całkowite, klucz podstawowy, make tekst do 100 znaków, wszystkie pola wymagane (bez null)
+
+-- Dodaj do tabeli pole model (tekst do 100 znaków, pole wymagane
+
+-- Jednym zapytaniem dodaj pole color i note
+
+-- Kolumna note powinna mieć tylko do 100 znaków Zmień to.
+
+-- Jednym zapytaniem zmień typ pola year i color (pole color ma zmienioną pozycję w tabeli)
+
+-- Zmięń nazwę pola note na vehicleCondition
+
+-- Usuń kolumnę vehicleCondition
+
+-- Zmień nazwę tabeli vehicles na cars
